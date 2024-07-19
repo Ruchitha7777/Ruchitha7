a3c4d42b53cbb3b1d729c3d0c7229584a89a11bc
Ruchitha <ruchithabruchithab@gmail.com>
2024-07-19 11:39:59 +0530

    initial commit

---
+++ Demo1.java
@@ -0,0 +1,21 @@
+class Arr
+{
+	public static void main(String s[])
+	{
+		String a[]=new String[10];
+		a[0]="anitha";
+		a[1]="liki";
+		a[2]="vishala";
+		a[3]="ruchita";
+		a[4]="revathi";
+		a[5]="suchitra";
+		a[6]="sanjana";
+		a[7]="sangeetha";
+		a[8]="shravani";
+		a[9]="supriya";
+		for(int i=0;i<=9;i++)
+			{
+				System.out.println("a["+i+"]="+a[i]);
+			}
+	}
+}
